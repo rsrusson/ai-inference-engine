57dd2ad2f1629730e172d47fd56744e9d8f2131f
rsrusson <rsrusson1@gmail.com>
2026-09-06 13:18:36 -0600

    vllm startup/shutdown and endpoint references added

--- main-torch.py
+++ main-torch.py
@@ -147,4 +147,4 @@ async def generate(req: GenerateRequest) -> GenerateResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main-torch:app", host="0.0.0.0", port=8001, reload=False)
